31fefef7f9eb2f814c8fc8a544f8e76eb91da478
YuShifan <894402575bt@gmail.com>
2023-12-17 23:57:18 +0800

    feat: stream support

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,61 @@
 <script setup>
-import { ref } from 'vue'
-import axios from 'axios'
+import { ref, nextTick } from 'vue'
 
-console.log(import.meta.env)
-const http = axios.create({
-  baseURL: 'https://api.openai.com/v1/chat',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_OPEN_API_KEY}`,
-    'OpenAI-Organization': import.meta.env.VITE_ORG_ID,
-  },
-})
 const content = ref('')
 const BTN_TEXT = 'Submit 🚀'
 const res = ref('✅ The answer will be displayed here.')
 const btnText = ref(BTN_TEXT)
 
-async function postData() {
+async function createCompletionsChat() {
   try {
     btnText.value = 'Thinking...🤔'
-    const response = await http.post('/completions', {
+
+    const userMessages = [{ role: 'user', content: content.value }]
+
+    const requestData = JSON.stringify({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: content.value }],
-      temperature: 0.7,
+      messages: userMessages,
+      stream: true,
     })
-    console.log(response)
-    res.value = response.data.choices[0].message.content
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPEN_API_KEY}`,
+      },
+      body: requestData,
+    }
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', fetchOptions)
+    const reader = response.body.getReader()
+    res.value = ''
+
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) break
+
+      const chunkStr = new TextDecoder('utf-8').decode(value)
+      const lines = chunkStr
+        .split('\n')
+        .filter((line) => line !== '' && line.length > 0)
+        .map((line) => line.replace(/^data: /, '').trim())
+        .filter((line) => line !== '[DONE]')
+        .map((line) => JSON.parse(line))
+      for (const line of lines) {
+        const {
+          choices: [
+            {
+              delta: { content },
+            },
+          ],
+        } = line
+        if (content) {
+          res.value += content
+        }
+      }
+    }
+    console.log('Stream ended.')
   } catch (error) {
     console.error(error)
     res.value = error.response.data.error.message
@@ -35,7 +65,7 @@ async function postData() {
 }
 
 const askAi = () => {
-  postData()
+  createCompletionsChat()
 }
 </script>
 
